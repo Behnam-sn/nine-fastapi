45f6d733bdd69069903c4db8c472b45eb3a4183d
behnam <salehinezhad.b@gmail.com>
2022-07-30 09:53:55 +0430

    update alembic config

--- alembic/env.py
+++ alembic/env.py
@@ -1,9 +1,18 @@
+import os
+import sys
 from logging.config import fileConfig
 
-from sqlalchemy import engine_from_config
-from sqlalchemy import pool
+from dotenv import load_dotenv
+from sqlalchemy import engine_from_config, pool
+from src.database.base import Base
 
 from alembic import context
+
+if os.getenv("DATABASE_URL") is None:
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    load_dotenv(os.path.join(BASE_DIR, '.env'))
+    sys.path.append(BASE_DIR)
+
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -18,7 +27,16 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+# target_metadata = None
+
+url = os.getenv("DATABASE_URL")
+if url and url.startswith("postgres://"):
+    url = url.replace("postgres://", "postgresql://", 1)
+
+config.set_main_option('sqlalchemy.url', url)
+
+target_metadata = Base.metadata
+
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
