1559d9f7c62ef78500ab8272eff519c25c33ae2a
behnam <salehinezhad.b@gmail.com>
2022-07-30 09:33:55 +0430

    add api deps

---
+++ src/api/deps.py
@@ -0,0 +1,59 @@
+from typing import Generator
+
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from jose import JWTError, jwt
+from sqlalchemy.orm import Session
+from src import crud, models, schemas
+from src.core import security
+from src.core.config import settings
+from src.database.session import SessionLocal
+
+oauth2_scheme = OAuth2PasswordBearer(
+    tokenUrl=f"{settings.API_V1_STR}/auth/login"
+)
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+def get_db() -> Generator:
+    try:
+        db = SessionLocal()
+        yield db
+
+    finally:
+        db.close()
+
+
+async def get_current_user(db: Session = Depends(get_db), token: str = Depends(oauth2_scheme)) -> models.User:
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+
+    try:
+        payload = jwt.decode(
+            token, settings.SECRET_KEY, algorithms=[security.ALGORITHM]
+        )
+
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+
+        token_data = schemas.TokenData(username=username)
+
+    except JWTError as e:
+        raise credentials_exception from e
+
+    user = crud.user.get_user_by_username(db, username=token_data.username)
+    if user is None:
+        raise credentials_exception
+
+    return user
+
+
+async def get_current_active_user(current_user: models.User = Depends(get_current_user)):
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
