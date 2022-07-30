6fa5b4bd65cb332fa99cbe85cbdd452557f8991e
behnam <salehinezhad.b@gmail.com>
2022-07-30 09:33:25 +0430

    add auth routes

---
+++ src/api/api_v1/endpoints/auth.py
@@ -0,0 +1,63 @@
+from datetime import timedelta
+
+from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
+from sqlalchemy.orm import Session
+from src import crud, models, schemas
+from src.api import deps
+from src.core import security
+from src.core.config import settings
+
+router = APIRouter()
+
+
+@router.post("/signup")
+def create_user(
+    user: schemas.UserCreate,
+    db: Session = Depends(deps.get_db)
+):
+
+    if crud.get_user_by_username(db, username=user.username):
+        raise HTTPException(
+            status_code=400,
+            detail="Username already registered"
+        )
+
+    crud.create_user(db=db, user=user)
+
+
+@router.post("/login", response_model=schemas.Token)
+def login(
+    form_data: OAuth2PasswordRequestForm = Depends(),
+    db: Session = Depends(deps.get_db)
+):
+
+    user = crud.user.authenticate_user(
+        db,
+        username=form_data.username,
+        password=form_data.password
+    )
+
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+    access_token_expires = timedelta(
+        minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES
+    )
+    access_token = security.create_access_token(
+        data={"sub": user.username},
+        expires_delta=access_token_expires
+    )
+
+    return {"access_token": access_token, "token_type": "bearer"}
+
+
+@router.post("/test-token", response_model=schemas.User)
+def test_token(
+    current_user: models.User = Depends(deps.get_current_user)
+):
+    return current_user
