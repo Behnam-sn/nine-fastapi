645b11d50700fdd7f36773733a6d68ac972f364c
behnam <salehinezhad.b@gmail.com>
2022-07-30 17:15:33 +0430

    minor change

--- src/api/deps.py
+++ src/api/deps.py
@@ -13,8 +13,6 @@ oauth2_scheme = OAuth2PasswordBearer(
     tokenUrl=f"{settings.API_V1_STR}/auth/login"
 )
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
-
 
 def get_db() -> Generator:
     try:
@@ -54,6 +52,6 @@ async def get_current_user(db: Session = Depends(get_db), token: str = Depends(o
 
 
 async def get_current_active_user(current_user: models.User = Depends(get_current_user)):
-    if current_user.disabled:
-        raise HTTPException(status_code=400, detail="Inactive user")
+    # if current_user.disabled:
+    #     raise HTTPException(status_code=400, detail="Inactive user")
     return current_user
