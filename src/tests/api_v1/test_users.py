2f8af5010e5d5da66fad2a2dff7f4c52b5274c41
behnam <salehinezhad.b@gmail.com>
2022-07-31 20:05:29 +0430

    add users tests

---
+++ src/tests/api_v1/test_users.py
@@ -0,0 +1,103 @@
+from src.core.config import settings
+from src.tests.conftest import client
+from src.tests.utils import (create_random_user, random_lower_string,
+                             user_authentication_headers)
+
+
+def test_get_current_user():
+    username = random_lower_string()
+    password = random_lower_string()
+
+    create_random_user(username=username, password=password)
+    token = user_authentication_headers(username=username, password=password)
+
+    response = client.get(
+        f"{settings.API_V1_STR}/users/current-user",
+        headers=token,
+    )
+
+    assert response.status_code == 200
+
+
+def test_get_user_by_username():
+    username = random_lower_string()
+
+    create_random_user(username=username)
+
+    response = client.get(
+        f"{settings.API_V1_STR}/users/{username}",
+    )
+    user = response.json()
+
+    assert response.status_code == 200
+    assert user["username"] == username
+
+
+def test_get_all_users():
+    create_random_user()
+
+    response = client.get(
+        f"{settings.API_V1_STR}/users/all",
+    )
+
+    assert response.status_code == 200
+
+
+def test_update_user():
+    username = random_lower_string()
+    password = random_lower_string()
+
+    create_random_user(username=username, password=password)
+    token = user_authentication_headers(username=username, password=password)
+
+    data = {
+        "username": random_lower_string(),
+        "name": random_lower_string(),
+        "bio": random_lower_string()
+    }
+
+    response = client.put(
+        f"{settings.API_V1_STR}/users/",
+        headers=token,
+        json=data,
+    )
+    user = response.json()
+
+    assert response.status_code == 200
+    assert user["username"] == data["username"]
+    assert user["name"] == data["name"]
+    assert user["bio"] == data["bio"]
+
+
+def test_activate_user():
+    username = random_lower_string()
+    password = random_lower_string()
+
+    create_random_user(username=username, password=password)
+    token = user_authentication_headers(username=username, password=password)
+
+    response = client.put(
+        f"{settings.API_V1_STR}/users/activate/{username}",
+        headers=token,
+    )
+    user = response.json()
+
+    assert response.status_code == 200
+    assert user["is_active"] == True
+
+
+def test_deactivate_user():
+    username = random_lower_string()
+    password = random_lower_string()
+
+    create_random_user(username=username, password=password)
+    token = user_authentication_headers(username=username, password=password)
+
+    response = client.put(
+        f"{settings.API_V1_STR}/users/deactivate/{username}",
+        headers=token,
+    )
+    user = response.json()
+
+    assert response.status_code == 200
+    assert user["is_active"] == False
