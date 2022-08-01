45ecdd26dfd049bd710b1971c2c61f5859bef6b5
behnam <salehinezhad.b@gmail.com>
2022-08-01 13:38:33 +0430

    minor change

--- src/api/api_v1/api.py
+++ src/api/api_v1/api.py
@@ -3,5 +3,5 @@ from src.api.api_v1.endpoints import auth, posts, users
 
 api_router = APIRouter()
 api_router.include_router(auth.router, prefix="/auth", tags=["auth"])
-api_router.include_router(posts.router, prefix="/posts", tags=["posts"])
 api_router.include_router(users.router, prefix="/users", tags=["users"])
+api_router.include_router(posts.router, prefix="/posts", tags=["posts"])
