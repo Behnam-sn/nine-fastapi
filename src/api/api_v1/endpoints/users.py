f93de7d7b246ab61a89dc441538426d57298ddc3
behnam <salehinezhad.b@gmail.com>
2022-08-23 14:54:08 +0430

    disabled author

--- src/api/api_v1/endpoints/users.py
+++ src/api/api_v1/endpoints/users.py
@@ -54,20 +54,20 @@ def get_user_by_username(
     return db_user
 
 
-@router.get("/author/{id}", response_model=schemas.Author)
-def get_author_by_id(
-    id: int,
-    db: Session = Depends(deps.get_db),
-):
-    db_user = crud.user.get_by_id(db, id=id)
+# @router.get("/author/{id}", response_model=schemas.Author)
+# def get_author_by_id(
+#     id: int,
+#     db: Session = Depends(deps.get_db),
+# ):
+#     db_user = crud.user.get_by_id(db, id=id)
 
-    if db_user is None:
-        raise HTTPException(status_code=404, detail="User not found")
+#     if db_user is None:
+#         raise HTTPException(status_code=404, detail="User not found")
 
-    if not db_user.is_active:
-        raise HTTPException(status_code=400, detail="User is not active")
+#     if not db_user.is_active:
+#         raise HTTPException(status_code=400, detail="User is not active")
 
-    return db_user
+#     return db_user
 
 
 @router.put("/", response_model=schemas.User)
