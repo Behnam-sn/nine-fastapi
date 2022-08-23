ef400ffbd74277b135ca11da3a73d9c9a5ca273b
behnam <salehinezhad.b@gmail.com>
2022-08-23 15:12:37 +0430

    update likes

--- src/api/api_v1/endpoints/likes.py
+++ src/api/api_v1/endpoints/likes.py
@@ -6,7 +6,7 @@ from src.api import deps
 router = APIRouter()
 
 
-@router.get("/", response_model=list[schemas.Like])
+@router.get("/all", response_model=list[schemas.Like])
 def get_all_likes(
     skip: int = 0,
     limit: int = 100,
@@ -15,7 +15,76 @@ def get_all_likes(
     return crud.like.get_all(db, skip=skip, limit=limit)
 
 
-@router.post("/post/{post_id}", response_model=schemas.PostLike)
+@router.get("/{id}", response_model=schemas.Like)
+def get_like_by_id(
+    id: int,
+    db: Session = Depends(deps.get_db),
+):
+    db_like = crud.like.get_by_id(db, id=id)
+
+    if db_like is None:
+        raise HTTPException(status_code=404, detail="Like not found")
+
+    return db_like
+
+
+@router.get("/owner/{owner_id}/count", response_model=int)
+def get_likes_count_by_owner_id(
+    owner_id: int,
+    db: Session = Depends(deps.get_db)
+):
+    db_user = crud.user.get_by_id(db, id=owner_id)
+
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    return crud.like.get_likes_count_by_owner_id(db, owner_id=owner_id)
+
+
+@router.get("/owner/{owner_id}", response_model=list[schemas.Id])
+def get_likes_ids_by_owner_id(
+    owner_id: int,
+    skip: int = 0,
+    limit: int = 100,
+    db: Session = Depends(deps.get_db),
+):
+    db_user = crud.user.get_by_id(db, id=owner_id)
+
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    return crud.like.get_likes_by_owner_id(db, skip=skip, limit=limit, owner_id=owner_id)
+
+
+@router.get("/post/{post_id}/count", response_model=int)
+def get_likes_count_by_post_id(
+    post_id: int,
+    db: Session = Depends(deps.get_db)
+):
+    db_post = crud.post.get_by_id(db, id=post_id)
+
+    if db_post is None:
+        raise HTTPException(status_code=404, detail="Post not found")
+
+    return crud.like.get_likes_count_by_post_id(db, post_id=post_id)
+
+
+@router.get("/post/{post_id}", response_model=list[schemas.Id])
+def get_likes_ids_by_post_id(
+    post_id: int,
+    skip: int = 0,
+    limit: int = 100,
+    db: Session = Depends(deps.get_db),
+):
+    db_post = crud.post.get_by_id(db, id=post_id)
+
+    if db_post is None:
+        raise HTTPException(status_code=404, detail="Post not found")
+
+    return crud.like.get_likes_by_post_id(db, skip=skip, limit=limit, post_id=post_id)
+
+
+@router.post("/post/{post_id}", response_model=schemas.Like)
 def like_post(
     post_id: int,
     current_user: models.User = Depends(deps.get_current_user),
@@ -38,7 +107,7 @@ def like_post(
     return crud.like.like_post(db, post_id=post_id, owner_id=current_user.id)
 
 
-@router.delete("/post/{post_id}", response_model=schemas.PostLike)
+@router.delete("/post/{post_id}", response_model=schemas.Like)
 def unlike_post(
     post_id: int,
     current_user: models.User = Depends(deps.get_current_user),
@@ -61,7 +130,35 @@ def unlike_post(
     return crud.like.unlike_post(db, post_id=post_id, owner_id=current_user.id)
 
 
-@router.post("/comment/{comment_id}", response_model=schemas.CommentLike)
+@router.get("/comment/{comment_id}/count", response_model=int)
+def get_likes_count_by_comment_id(
+    comment_id: int,
+    db: Session = Depends(deps.get_db)
+):
+    db_comment = crud.comment.get_by_id(db, id=comment_id)
+
+    if db_comment is None:
+        raise HTTPException(status_code=404, detail="Comment not found")
+
+    return crud.like.get_likes_count_by_comment_id(db, comment_id=comment_id)
+
+
+@router.get("/comment/{comment_id}", response_model=list[schemas.Id])
+def get_likes_ids_by_post_id(
+    comment_id: int,
+    skip: int = 0,
+    limit: int = 100,
+    db: Session = Depends(deps.get_db),
+):
+    db_comment = crud.comment.get_by_id(db, id=comment_id)
+
+    if db_comment is None:
+        raise HTTPException(status_code=404, detail="Comment not found")
+
+    return crud.like.get_likes_by_comment_id(db, comment_id=comment_id, skip=skip, limit=limit)
+
+
+@router.post("/comment/{comment_id}", response_model=schemas.Like)
 def like_comment(
     comment_id: int,
     current_user: models.User = Depends(deps.get_current_user),
@@ -84,7 +181,7 @@ def like_comment(
     return crud.like.like_comment(db, comment_id=comment_id, owner_id=current_user.id)
 
 
-@router.delete("/comment/{comment_id}", response_model=schemas.CommentLike)
+@router.delete("/comment/{comment_id}", response_model=schemas.Like)
 def unlike_comment(
     comment_id: int,
     current_user: models.User = Depends(deps.get_current_user),
--- src/crud/like.py
+++ src/crud/like.py
@@ -26,7 +26,23 @@ class Like():
     def get_all(self, db: Session, skip: int = 0, limit: int = 100) -> list[models.Like]:
         return (
             db.query(models.Like)
-            .order_by(models.Like.id.desc())
+            .order_by(models.Like.id)
+            .offset(skip)
+            .limit(limit)
+            .all()
+        )
+
+    def get_by_id(self, db: Session, id: id) -> models.Like | None:
+        return db.query(models.Like).filter(models.Like.id == id).first()
+
+    def get_count_by_owner_id(self, db: Session, owner_id: int) -> int:
+        return db.query(models.Like).filter(models.Like.owner_id == owner_id).count()
+
+    def get_by_owner_id(self, db: Session, owner_id: int, skip: int = 0, limit: int = 100) -> list[models.Like]:
+        return (
+            db.query(models.Like)
+            .filter(models.Like.owner_id == owner_id)
+            .order_by(models.Like.id)
             .offset(skip)
             .limit(limit)
             .all()
@@ -39,11 +55,37 @@ class Like():
             .first()
         )
 
+    def get_count_by_post_id(self, db: Session, post_id: int) -> int:
+        return db.query(models.Like).filter(models.Like.post_id == post_id).count()
+
+    def get_by_post_id(self, db: Session, post_id: int, skip: int = 0, limit: int = 100) -> list[models.Like]:
+        return (
+            db.query(models.Like)
+            .filter(models.Like.post_id == post_id)
+            .order_by(models.Like.id)
+            .offset(skip)
+            .limit(limit)
+            .all()
+        )
+
     def get_comment_like(self, db: Session, comment_id: int, owner_id: int) -> models.Like | None:
         return (
             db.query(models.Like)
             .filter(models.Like.comment_id == comment_id, models.Like.owner_id == owner_id)
             .first()
+        )
+
+    def get_count_by_comment_id(self, db: Session, comment_id: int) -> int:
+        return db.query(models.Like).filter(models.Like.comment_id == comment_id).count()
+
+    def get_by_comment_id(self, db: Session, comment_id: int, skip: int = 0, limit: int = 100) -> list[models.Like]:
+        return (
+            db.query(models.Like)
+            .filter(models.Like.comment_id == comment_id)
+            .order_by(models.Like.id)
+            .offset(skip)
+            .limit(limit)
+            .all()
         )
 
     def unlike_post(self, db: Session, post_id: int, owner_id: int) -> models.Like:
