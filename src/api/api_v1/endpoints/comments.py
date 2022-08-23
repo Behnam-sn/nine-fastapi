035026e912c78528ff0cde4837b2330ad48ba0b0
behnam <salehinezhad.b@gmail.com>
2022-08-24 04:22:06 +0430

    rearrange routes

--- src/api/api_v1/endpoints/comments.py
+++ src/api/api_v1/endpoints/comments.py
@@ -6,6 +6,15 @@ from src.api import deps
 router = APIRouter()
 
 
+@router.get("/all", response_model=list[schemas.Comment])
+def get_all_comments(
+    skip: int = 0,
+    limit: int = 100,
+    db: Session = Depends(deps.get_db)
+):
+    return crud.comment.get_all(db, skip=skip, limit=limit)
+
+
 @router.post("/", response_model=schemas.Comment)
 def create_comment(
     comment: schemas.CommentCreate,
@@ -18,6 +27,88 @@ def create_comment(
         raise HTTPException(status_code=404, detail="Post not found")
 
     return crud.comment.create(db, comment=comment, owner_id=current_user.id)
+
+
+@router.put("/{id}", response_model=schemas.Comment)
+def update_comment(
+    id: int,
+    comment_update: schemas.CommentUpdate,
+    current_user: models.User = Depends(deps.get_current_user),
+    db: Session = Depends(deps.get_db),
+):
+    db_comment = crud.comment.get_by_id(db, id=id)
+
+    if db_comment is None:
+        raise HTTPException(status_code=404, detail="Comment not found")
+
+    if db_comment.owner_id != current_user.id:
+        raise HTTPException(status_code=400, detail="Permission Denied")
+
+    return crud.comment.update(db, id=id, comment_update=comment_update)
+
+
+@router.delete("/{id}")
+def delete_comment(
+    id: int,
+    current_user: models.User = Depends(deps.get_current_user),
+    db: Session = Depends(deps.get_db),
+):
+    db_comment = crud.comment.get_by_id(db, id=id)
+
+    if db_comment is None:
+        raise HTTPException(status_code=404, detail="Comment not found")
+
+    if not current_user.is_superuser:
+        raise HTTPException(status_code=400, detail="Permission Denied")
+
+    crud.comment.delete(db, id=id)
+
+
+@router.put("/activate/{id}", response_model=schemas.Comment)
+def activate_comment(
+    id: int,
+    current_user: models.User = Depends(deps.get_current_user),
+    db: Session = Depends(deps.get_db)
+):
+    db_comment = crud.comment.get_by_id(db, id=id)
+
+    if db_comment is None:
+        raise HTTPException(status_code=404, detail="Comment not found")
+
+    if not current_user.is_superuser:
+        raise HTTPException(status_code=400, detail="Not Authenticated")
+
+    return crud.comment.active(db, id=id)
+
+
+@router.put("/deactivate/{id}", response_model=schemas.Comment)
+def deactivate_comment(
+    id: int,
+    current_user: models.User = Depends(deps.get_current_user),
+    db: Session = Depends(deps.get_db)
+):
+    db_comment = crud.comment.get_by_id(db, id=id)
+
+    if db_comment is None:
+        raise HTTPException(status_code=404, detail="Comment not found")
+
+    if current_user.is_superuser is False and current_user.id != db_comment.owner_id:
+        raise HTTPException(status_code=400, detail="Not Authenticated")
+
+    return crud.comment.deactive(db, id=id)
+
+
+@router.get("/{id}", response_model=schemas.Comment)
+def get_comment_by_id(
+    id: int,
+    db: Session = Depends(deps.get_db),
+):
+    db_comment = crud.comment.get_by_id(db, id=id)
+
+    if db_comment is None:
+        raise HTTPException(status_code=404, detail="Comment not found")
+
+    return db_comment
 
 
 @router.get("/count", response_model=int)
@@ -34,28 +125,6 @@ def get_all_comments_ids(
     db: Session = Depends(deps.get_db)
 ):
     return crud.comment.get_all(db, skip=skip, limit=limit)
-
-
-@router.get("/all", response_model=list[schemas.Comment])
-def get_all_comments(
-    skip: int = 0,
-    limit: int = 100,
-    db: Session = Depends(deps.get_db)
-):
-    return crud.comment.get_all(db, skip=skip, limit=limit)
-
-
-@router.get("/{id}", response_model=schemas.Comment)
-def get_comment_by_id(
-    id: int,
-    db: Session = Depends(deps.get_db),
-):
-    db_comment = crud.comment.get_by_id(db, id=id)
-
-    if db_comment is None:
-        raise HTTPException(status_code=404, detail="Comment not found")
-
-    return db_comment
 
 
 @router.get("/owner/count/{owner_id}", response_model=int)
@@ -112,72 +181,3 @@ def get_comments_ids_by_post_id(
         raise HTTPException(status_code=404, detail="Post not found")
 
     return crud.comment.get_by_post_id(db, post_id=post_id, skip=skip, limit=limit)
-
-
-@router.put("/{id}", response_model=schemas.Comment)
-def update_comment(
-    id: int,
-    comment_update: schemas.CommentUpdate,
-    current_user: models.User = Depends(deps.get_current_user),
-    db: Session = Depends(deps.get_db),
-):
-    db_comment = crud.comment.get_by_id(db, id=id)
-
-    if db_comment is None:
-        raise HTTPException(status_code=404, detail="Comment not found")
-
-    if db_comment.owner_id != current_user.id:
-        raise HTTPException(status_code=400, detail="Permission Denied")
-
-    return crud.comment.update(db, id=id, comment_update=comment_update)
-
-
-@router.put("/activate/{id}", response_model=schemas.Comment)
-def activate_comment(
-    id: int,
-    current_user: models.User = Depends(deps.get_current_user),
-    db: Session = Depends(deps.get_db)
-):
-    db_comment = crud.comment.get_by_id(db, id=id)
-
-    if db_comment is None:
-        raise HTTPException(status_code=404, detail="Comment not found")
-
-    if not current_user.is_superuser:
-        raise HTTPException(status_code=400, detail="Not Authenticated")
-
-    return crud.comment.active(db, id=id)
-
-
-@router.put("/deactivate/{id}", response_model=schemas.Comment)
-def deactivate_comment(
-    id: int,
-    current_user: models.User = Depends(deps.get_current_user),
-    db: Session = Depends(deps.get_db)
-):
-    db_comment = crud.comment.get_by_id(db, id=id)
-
-    if db_comment is None:
-        raise HTTPException(status_code=404, detail="Comment not found")
-
-    if current_user.is_superuser is False and current_user.id != db_comment.owner_id:
-        raise HTTPException(status_code=400, detail="Not Authenticated")
-
-    return crud.comment.deactive(db, id=id)
-
-
-@router.delete("/{id}")
-def delete_comment(
-    id: int,
-    current_user: models.User = Depends(deps.get_current_user),
-    db: Session = Depends(deps.get_db),
-):
-    db_comment = crud.comment.get_by_id(db, id=id)
-
-    if db_comment is None:
-        raise HTTPException(status_code=404, detail="Comment not found")
-
-    if not current_user.is_superuser:
-        raise HTTPException(status_code=400, detail="Permission Denied")
-
-    crud.comment.delete(db, id=id)
