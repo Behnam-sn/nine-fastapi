c7c50670f1f0229e7ffa8cd2663bdcabde4939b9
behnam <salehinezhad.b@gmail.com>
2022-08-24 04:32:02 +0430

    rearrange routes

--- src/api/api_v1/endpoints/follows.py
+++ src/api/api_v1/endpoints/follows.py
@@ -28,6 +28,61 @@ def get_follow_by_id(
     return db_follow
 
 
+@router.post("/{following_id}", response_model=schemas.Follow)
+def follow_user(
+    following_id: int,
+    current_user: models.User = Depends(deps.get_current_user),
+    db: Session = Depends(deps.get_db),
+):
+    db_user = crud.user.get_by_id(db, id=following_id)
+
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    if following_id == current_user.id:
+        raise HTTPException(
+            status_code=400, detail="You can't follow yourself"
+        )
+
+    db_follow = crud.follow.get_follow(
+        db, follower_id=current_user.id, following_id=following_id
+    )
+
+    if db_follow is not None:
+        raise HTTPException(
+            status_code=400, detail="You already followed this user"
+        )
+
+    return crud.follow.follow(db, follower_id=current_user.id, following_id=following_id)
+
+
+@router.delete("/{following_id}", response_model=schemas.Follow)
+def unfollow_user(
+    following_id: int,
+    current_user: models.User = Depends(deps.get_current_user),
+    db: Session = Depends(deps.get_db),
+):
+    db_user = crud.user.get_by_id(db, id=following_id)
+
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    if following_id == current_user.id:
+        raise HTTPException(
+            status_code=400, detail="You can't unfollow yourself"
+        )
+    db_follow = crud.follow.get_follow(
+        db, follower_id=current_user.id, following_id=following_id
+    )
+
+    if db_follow is None:
+        raise HTTPException(
+            status_code=400, detail="You have not followed this user"
+        )
+
+    return crud.follow.unfollow(db, follower_id=current_user.id, following_id=following_id)
+
+
 @router.get("/follower/count/{follower_id}", response_model=int)
 def get_follows_count_follower_id(
     follower_id: int,
@@ -82,58 +137,3 @@ def get_follows_ids_by_following_id(
         raise HTTPException(status_code=404, detail="User not found")
 
     return crud.follow.get_by_following_id(db, following_id=following_id, skip=skip, limit=limit)
-
-
-@router.post("/{following_id}", response_model=schemas.Follow)
-def follow_user(
-    following_id: int,
-    current_user: models.User = Depends(deps.get_current_user),
-    db: Session = Depends(deps.get_db),
-):
-    db_user = crud.user.get_by_id(db, id=following_id)
-
-    if db_user is None:
-        raise HTTPException(status_code=404, detail="User not found")
-
-    if following_id == current_user.id:
-        raise HTTPException(
-            status_code=400, detail="You can't follow yourself"
-        )
-
-    db_follow = crud.follow.get_follow(
-        db, follower_id=current_user.id, following_id=following_id
-    )
-
-    if db_follow is not None:
-        raise HTTPException(
-            status_code=400, detail="You already followed this user"
-        )
-
-    return crud.follow.follow(db, follower_id=current_user.id, following_id=following_id)
-
-
-@router.delete("/{following_id}", response_model=schemas.Follow)
-def unfollow_user(
-    following_id: int,
-    current_user: models.User = Depends(deps.get_current_user),
-    db: Session = Depends(deps.get_db),
-):
-    db_user = crud.user.get_by_id(db, id=following_id)
-
-    if db_user is None:
-        raise HTTPException(status_code=404, detail="User not found")
-
-    if following_id == current_user.id:
-        raise HTTPException(
-            status_code=400, detail="You can't unfollow yourself"
-        )
-    db_follow = crud.follow.get_follow(
-        db, follower_id=current_user.id, following_id=following_id
-    )
-
-    if db_follow is None:
-        raise HTTPException(
-            status_code=400, detail="You have not followed this user"
-        )
-
-    return crud.follow.unfollow(db, follower_id=current_user.id, following_id=following_id)
--- src/api/api_v1/endpoints/posts.py
+++ src/api/api_v1/endpoints/posts.py
@@ -6,31 +6,6 @@ from src.api import deps
 router = APIRouter()
 
 
-@router.post("/", response_model=schemas.Post)
-def create_post(
-    post: schemas.PostCreate,
-    current_user: models.User = Depends(deps.get_current_user),
-    db: Session = Depends(deps.get_db),
-):
-    return crud.post.create(db, post=post, owner_id=current_user.id)
-
-
-@router.get("/count", response_model=int)
-def get_all_posts_count(
-    db: Session = Depends(deps.get_db)
-):
-    return crud.post.get_all_count(db)
-
-
-@router.get("/ids", response_model=list[schemas.Id])
-def get_all_posts_ids(
-    skip: int = 0,
-    limit: int = 100,
-    db: Session = Depends(deps.get_db)
-):
-    return crud.post.get_all(db, skip=skip, limit=limit)
-
-
 @router.get("/all", response_model=list[schemas.Post])
 def get_all_posts(
     skip: int = 0,
@@ -38,6 +13,15 @@ def get_all_posts(
     db: Session = Depends(deps.get_db)
 ):
     return crud.post.get_all(db, skip=skip, limit=limit)
+
+
+@router.post("/", response_model=schemas.Post)
+def create_post(
+    post: schemas.PostCreate,
+    current_user: models.User = Depends(deps.get_current_user),
+    db: Session = Depends(deps.get_db),
+):
+    return crud.post.create(db, post=post, owner_id=current_user.id)
 
 
 @router.get("/{id}", response_model=schemas.Post)
@@ -51,34 +35,6 @@ def get_post_by_id(
         raise HTTPException(status_code=404, detail="Post not found")
 
     return db_post
-
-
-@router.get("/owner/count/{owner_id}", response_model=int)
-def get_posts_count_by_owner_id(
-    owner_id: int,
-    db: Session = Depends(deps.get_db)
-):
-    db_user = crud.user.get_by_id(db, id=owner_id)
-
-    if db_user is None:
-        raise HTTPException(status_code=404, detail="User not found")
-
-    return crud.post.get_count_by_owner_id(db, owner_id=owner_id)
-
-
-@router.get("/owner/ids/{owner_id}", response_model=list[schemas.Id])
-def get_posts_ids_by_owner_id(
-    owner_id: int,
-    skip: int = 0,
-    limit: int = 100,
-    db: Session = Depends(deps.get_db),
-):
-    db_user = crud.user.get_by_id(db, id=owner_id)
-
-    if db_user is None:
-        raise HTTPException(status_code=404, detail="User not found")
-
-    return crud.post.get_by_owner_id(db, owner_id=owner_id, skip=skip, limit=limit)
 
 
 @router.put("/{id}", response_model=schemas.Post)
@@ -97,6 +53,23 @@ def update_post(
         raise HTTPException(status_code=400, detail="Permission Denied")
 
     return crud.post.update(db, id=id, post_update=post_update)
+
+
+@router.delete("/{id}")
+def delete_post(
+    id: int,
+    current_user: models.User = Depends(deps.get_current_user),
+    db: Session = Depends(deps.get_db),
+):
+    db_post = crud.post.get_by_id(db, id=id)
+
+    if db_post is None:
+        raise HTTPException(status_code=404, detail="Post not found")
+
+    if not current_user.is_superuser:
+        raise HTTPException(status_code=400, detail="Permission Denied")
+
+    crud.post.delete(db, id=id)
 
 
 @router.put("/activate/{id}", response_model=schemas.Post)
@@ -133,18 +106,45 @@ def deactivate_post(
     return crud.post.deactive(db, id=id)
 
 
-@router.delete("/{id}")
-def delete_post(
-    id: int,
-    current_user: models.User = Depends(deps.get_current_user),
+@router.get("/count", response_model=int)
+def get_all_posts_count(
+    db: Session = Depends(deps.get_db)
+):
+    return crud.post.get_all_count(db)
+
+
+@router.get("/ids", response_model=list[schemas.Id])
+def get_all_posts_ids(
+    skip: int = 0,
+    limit: int = 100,
+    db: Session = Depends(deps.get_db)
+):
+    return crud.post.get_all(db, skip=skip, limit=limit)
+
+
+@router.get("/owner/count/{owner_id}", response_model=int)
+def get_posts_count_by_owner_id(
+    owner_id: int,
+    db: Session = Depends(deps.get_db)
+):
+    db_user = crud.user.get_by_id(db, id=owner_id)
+
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    return crud.post.get_count_by_owner_id(db, owner_id=owner_id)
+
+
+@router.get("/owner/ids/{owner_id}", response_model=list[schemas.Id])
+def get_posts_ids_by_owner_id(
+    owner_id: int,
+    skip: int = 0,
+    limit: int = 100,
     db: Session = Depends(deps.get_db),
 ):
-    db_post = crud.post.get_by_id(db, id=id)
+    db_user = crud.user.get_by_id(db, id=owner_id)
 
-    if db_post is None:
-        raise HTTPException(status_code=404, detail="Post not found")
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
 
-    if not current_user.is_superuser:
-        raise HTTPException(status_code=400, detail="Permission Denied")
-
-    crud.post.delete(db, id=id)
+    return crud.post.get_by_owner_id(db, owner_id=owner_id, skip=skip, limit=limit)
