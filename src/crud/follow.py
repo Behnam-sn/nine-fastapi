00710faa694086e106aa30fe5254cb3c278b88fb
behnam <salehinezhad.b@gmail.com>
2022-08-24 04:35:53 +0430

    minor changes

--- src/crud/follow.py
+++ src/crud/follow.py
@@ -13,6 +13,14 @@ class Follow():
         db.refresh(db_follow)
         return db_follow
 
+    def unfollow(self, db: Session, follower_id: int, following_id: int) -> models.Follow:
+        db_follow = self.get_follow(
+            db, follower_id=follower_id, following_id=following_id
+        )
+        db.delete(db_follow)
+        db.commit()
+        return db_follow
+
     def get_all(self, db: Session, skip: int = 0, limit: int = 100) -> list[models.Follow]:
         return (
             db.query(models.Follow)
@@ -28,8 +36,19 @@ class Follow():
             .first()
         )
 
+    def get_follow(self, db: Session, follower_id: int, following_id: int) -> models.Follow | None:
+        return (
+            db.query(models.Follow)
+            .filter(models.Follow.follower_id == follower_id, models.Follow.following_id == following_id)
+            .first()
+        )
+
     def get_count_by_follower_id(self, db: Session, follower_id: int) -> int:
-        return db.query(models.Follow).filter(models.Follow.follower_id == follower_id).count()
+        return (
+            db.query(models.Follow)
+            .filter(models.Follow.follower_id == follower_id)
+            .count()
+        )
 
     def get_by_follower_id(self, db: Session, follower_id: int, skip: int = 0, limit: int = 100) -> list[models.Follow]:
         return (
@@ -42,7 +61,11 @@ class Follow():
         )
 
     def get_count_by_following_id(self, db: Session, following_id: int) -> int:
-        return db.query(models.Follow).filter(models.Follow.following_id == following_id).count()
+        return (
+            db.query(models.Follow)
+            .filter(models.Follow.following_id == following_id)
+            .count()
+        )
 
     def get_by_following_id(self, db: Session, following_id: int, skip: int = 0, limit: int = 100) -> list[models.Follow]:
         return (
@@ -53,21 +76,6 @@ class Follow():
             .limit(limit)
             .all()
         )
-
-    def get_follow(self, db: Session, follower_id: int, following_id: int) -> models.Follow | None:
-        return (
-            db.query(models.Follow)
-            .filter(models.Follow.follower_id == follower_id, models.Follow.following_id == following_id)
-            .first()
-        )
-
-    def unfollow(self, db: Session, follower_id: int, following_id: int) -> models.Follow:
-        db_follow = self.get_follow(
-            db, follower_id=follower_id, following_id=following_id
-        )
-        db.delete(db_follow)
-        db.commit()
-        return db_follow
 
 
 follow = Follow()
